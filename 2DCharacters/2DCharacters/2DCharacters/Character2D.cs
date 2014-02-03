9e0e3bead6db50c9f02be747195d5ea6eb280a6d
Ryan Watkins <rwatk001@ucr.edu>
2014-02-02 21:16:14 -0800

    2D Animated Sprite Class
    C# using XNA

---
+++ 2DCharacters/2DCharacters/2DCharacters/Character2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace _2DCharacters
+{
+    class Character2D
+    {
+        private Dictionary<String, AnimatedSprite> SpriteSheets;
+
+        public Character2D() { }
+
+        public Character2D(Dictionary<String, AnimatedSprite> spriteSheets)
+        {
+            SpriteSheets = spriteSheets;
+        }
+
+        public void AddSpriteSheet(String spriteState, AnimatedSprite spriteSheet)
+        {
+            SpriteSheets.Add(spriteState, spriteSheet);
+        }
+    }
+}
---
+++ 2DCharacters/2DCharacters/2DCharacters/Game1.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace _2DCharacters
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private AnimatedSprite animatedSprite;
+
+        private const int WINDOW_HEIGHT = 600;
+        private const  int WINDOW_WIDTH = 600;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D texture = Content.Load<Texture2D>("kirby_wait");
+            animatedSprite = new AnimatedSprite(texture, 1, 2);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            animatedSprite.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            animatedSprite.Draw(spriteBatch, new Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
+
+            base.Draw(gameTime);
+        }
+    }
+}
